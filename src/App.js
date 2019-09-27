2e2e2a8fa36111573906884809ebce81787e94fc
geleeroyale <krassvs@gmail.com>
2019-09-27 20:27:41 +0200

    remove filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import ApolloClient, { gql, InMemoryCache } from 'apollo-boost'
-import { ApolloProvider, Query } from 'react-apollo'
+import React, { Component } from 'react';
+import ApolloClient, { gql, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
 import {
   Grid,
   LinearProgress,
@@ -10,21 +10,23 @@ import {
   DialogContentText,
   DialogTitle,
   Button
-} from '@material-ui/core'
-import './App.css'
-import Header from './components/Header'
-import Error from './components/Error'
-import Filter from './components/Filter'
-import GivethDonators from './components/Visualisation/GivethDonators'
+} from '@material-ui/core';
+import './App.css';
+import Header from './components/Header';
+import Error from './components/Error';
+import Filter from './components/Filter';
+import GivethDonators from './components/Visualisation/GivethDonators';
 
 if (!process.env.REACT_APP_GRAPHQL_ENDPOINT) {
-  throw new Error('REACT_APP_GRAPHQL_ENDPOINT environment variable not defined')
+  throw new Error(
+    'REACT_APP_GRAPHQL_ENDPOINT environment variable not defined'
+  );
 }
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
   cache: new InMemoryCache()
-})
+});
 
 const DONATION_QUERY = gql`
   query givethdonations {
@@ -36,58 +38,39 @@ const DONATION_QUERY = gql`
       amount
     }
   }
-`
+`;
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       withImage: false,
       withName: false,
       orderBy: 'displayName',
       showHelpDialog: false
-    }
+    };
   }
 
   toggleHelpDialog = () => {
     this.setState(state => ({
       ...state,
       showHelpDialog: !state.showHelpDialog
-    }))
-  }
+    }));
+  };
 
   gotoQuickStartGuide = () => {
-    window.location.href = 'https://beta.giveth.io'
-  }
+    window.location.href = 'https://beta.giveth.io';
+  };
 
   render() {
-    const { withImage, withName, orderBy, showHelpDialog } = this.state
+    const { withImage, withName, orderBy, showHelpDialog } = this.state;
 
     return (
       <ApolloProvider client={client}>
         <div className="App">
           <Grid container direction="column">
             <Header onHelp={this.toggleHelpDialog} />
-            <Filter
-              orderBy={orderBy}
-              withImage={withImage}
-              withName={withName}
-              onOrderBy={field =>
-                this.setState(state => ({ ...state, orderBy: field }))
-              }
-              onToggleWithImage={() =>
-                this.setState(state => ({
-                  ...state,
-                  withImage: !state.withImage
-                }))
-              }
-              onToggleWithName={() =>
-                this.setState(state => ({
-                  ...state,
-                  withName: !state.withName
-                }))
-              }
-            />
+
             <Grid item>
               <Grid container>
                 <Query
@@ -109,8 +92,8 @@ class App extends Component {
                     ) : error ? (
                       <Error error={error} />
                     ) : (
-                      <GivethDonators donationData={data.donates}  />
-                    )
+                      <GivethDonators donationData={data.donates} />
+                    );
                   }}
                 </Query>
               </Grid>
@@ -146,8 +129,8 @@ class App extends Component {
           </Dialog>
         </div>
       </ApolloProvider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
